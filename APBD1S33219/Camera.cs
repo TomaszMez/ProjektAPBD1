181163fb869bb20abb3b5e7a77afc6f683f5199d
TomaszMez <tomtablet123456@gmail.com>
2026-03-18 17:38:23 +0100

    Dodano klasy dziedziczace po Device: Laptop, Projector, Camera reprezentujace rozne typy urzadzen

---
+++ APBD1S33219/Camera.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD1S33219;
+
+public class Camera : Device
+{
+    public int Resolution { get; }
+    public bool LensExchangeable { get; }
+    
+    public Camera(string name, Manufacturer manufacturer, int price, int resolution, bool lensExchangeable) : base(name, manufacturer, price)
+    {
+        Resolution = resolution;
+        LensExchangeable = lensExchangeable;
+    }
+}
