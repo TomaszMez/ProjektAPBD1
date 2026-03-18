0648a9afefb942a9af7b8a2dd551f3f300e19adf
TomaszMez <tomtablet123456@gmail.com>
2026-03-18 17:58:18 +0100

    Utworzenie klasy User reprezentujacej uzytkownika i enum UserType sluzacy do rozrozniania miedzy typami uzytkownikow

---
+++ APBD1S33219/User.cs
@@ -0,0 +1,19 @@
+﻿namespace APBD1S33219;
+
+public class User
+{
+    private static int _idCounter = 0;
+    
+    public int Id { get; }
+    public string Name { get; }
+    public string Surname { get; }
+    public UserType UserType { get; }
+
+    public User(string name, string surname, UserType userType)
+    {
+        Id = _idCounter++;
+        Name = name;
+        Surname = surname;
+        UserType = userType;
+    }
+}
---
+++ APBD1S33219/UserType.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD1S33219;
+
+public enum UserType
+{
+    Student,
+    Employee
+}
