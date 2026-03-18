d6b5044b3902c43463cc39782da23b50478ba2c2
TomaszMez <tomtablet123456@gmail.com>
2026-03-18 20:00:19 +0100

    Utworzenie klasy Rental sluzacej do sledzenia wypozyczen uzytkownikow

---
+++ APBD1S33219/Rental.cs
@@ -0,0 +1,28 @@
+﻿namespace APBD1S33219;
+
+public class Rental
+{
+    public User RUser { get; }
+    public Device RDevice { get; }
+    public DateTime RentalDate { get; }
+    public TimeSpan RentalDuration { get; }
+    public DateTime? ReturnDate { get; private set; }
+    public bool? IsOnTime { get; private set; }
+
+    public Rental(User user, Device device, DateTime rentalDate, TimeSpan rentalDuration)
+    {
+        RUser = user;
+        RDevice = device;
+        RentalDate = rentalDate;
+        RentalDuration = rentalDuration;
+    }
+
+    public void returnDevice()
+    {
+        ReturnDate = DateTime.Now;
+        if (ReturnDate > RentalDate + RentalDuration) IsOnTime = false;
+        else IsOnTime = true;
+    }
+    
+    
+}
